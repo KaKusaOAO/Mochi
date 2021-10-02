492fc477f56d37be2eef8fd77c03e492a7078528
ItsArcal139 <zapto139@gmail.com>
2021-10-02 22:31:08 +0800

    KaLib: Updates

--- KaLib/Utils/Links.cs
+++ KaLib/Utils/Links.cs
@@ -6,5 +6,7 @@ namespace KaLib.Utils
     {
         public static string TTS(string language, string content) =>
             $"https://translate.google.com/translate_tts?ie=UTF-8&tl={language}&client=tw-ob&q={UrlEncoder.Default.Encode(content)}";
+
+        public static string Twitter(string username) => $"https://twitter.com/{username}";
     }
 }
