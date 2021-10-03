26fc8015ddd881508097fda706895f580fadf014
ItsArcal139 <zapto139@gmail.com>
2021-10-03 22:22:52 +0800

    KaLib: Updates

--- KaLib/Utils/Links.cs
+++ KaLib/Utils/Links.cs
@@ -8,5 +8,7 @@ namespace KaLib.Utils
             $"https://translate.google.com/translate_tts?ie=UTF-8&tl={language}&client=tw-ob&q={UrlEncoder.Default.Encode(content)}";
 
         public static string Twitter(string username) => $"https://twitter.com/{username}";
+
+        public static string YouTubeChannel(string channelId) => $"https://youtube.com/c/${channelId}";
     }
 }
